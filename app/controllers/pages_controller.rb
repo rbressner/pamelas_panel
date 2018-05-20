509e49a373392e05b70c535c3bc071d79d48b7d9
Rachel Bressner <rachel.bressner@gmail.com>
2018-05-20 15:51:31 -0400

    im not as motivated as i was yesterday

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,19 @@
 class PagesController < ApplicationController
   def index
-    @admin = Admin.find_by(username: params[:username])
-    @teacher = Teacher.find_by(username: params[:username])
-    @student = Student.find_by(username: params[:username])
-    if @admin && @admin.password == params[:password] && @admin.user_type == params[:user_type]
-      session[:admin_id] = @admin.id
-      redirect_to(admins_path)
-    elsif @teacher && @teacher.password == params[:password] && @teacher.user_type == params[:user_type]
-      session[:teacher_id] = @teacher.id
-      redirect_to(teachers_path)
-    elsif @student && @student.password == params[:password] && @student.user_type == params[:user_type]
-      session[:student_id] = @student.id
-      redirect_to(students_path)
-    else
-      flash[:notice] = "Sorry, one or more of the fields is incorrect"
+    @welcome_words = ["Action", "Believe", "Clarity", "Dare",
+      "Encourage", "Hope", "Think", "Understand", "Value", "Excellence",
+      "Creative", "Dashing", "Improve", "Joy", "Kindness", "Mindfulness",
+      "Practice", "Smile", "Shine", "Laugh", "Dream",
+      "Positivity", "Nurture", "Listen"]
+      @words = @welcome_words.shuffle.first
+
     end
-  end
-end
+
+    # def words
+    #   welcome_words = ["Action", "Believe", "Clarity", "Dare",
+    #     "Encourage", "Hope", "Think", "Understand", "Value", "Excellence",
+    #     "Creative", "Dashing", "Improve", "Joy", "Kindness", "Mindfulness",
+    #     "Practice", "Smile", "Shine", "Laugh", "Dream",
+    #     "Positivity", "Nurture", "Listen"]
+    #   end
+    end
