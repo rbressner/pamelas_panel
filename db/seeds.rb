8ab5d0274a3e0b9fb47ad1aa931fc7e44a608300
Rachel Bressner <rachel.bressner@gmail.com>
2018-06-04 16:36:44 -0400

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  Admin.new(username: "rb", first_name: "ray", last_name: "b", email: "ray@b.com", password: "123456")
